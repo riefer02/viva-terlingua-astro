4dde27ef7473e5a9098de2bcde761745fc693662
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2025-01-18 16:08:49 -0600

    minor cleanup

--- src/lib/api/events.ts
+++ src/lib/api/events.ts
@@ -1,43 +1,5 @@
 import { api } from "./client";
-import type { QueryParams } from "./types";
-
-export interface Event {
-  id: number;
-  attributes: {
-    title: string;
-    description: string;
-    startDateTime: string;
-    endDateTime: string;
-    slug: string;
-    marqueeImage: {
-      data: {
-        attributes: {
-          url: string;
-          formats: Record<string, any>;
-        };
-      };
-    };
-    squareImage: {
-      data: {
-        attributes: {
-          url: string;
-          formats: Record<string, any>;
-        };
-      };
-    };
-    panelImage: {
-      data: {
-        attributes: {
-          url: string;
-          formats: Record<string, any>;
-        };
-      };
-    };
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-  };
-}
+import type { QueryParams, Event } from "./types";
 
 export const getEvents = async (params?: QueryParams) => {
   return api.get<Event[]>("events", {
