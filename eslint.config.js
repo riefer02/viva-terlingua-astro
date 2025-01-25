75e8ef5e92c72bdde387052e528b60522cdb11c6
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2025-01-25 11:08:45 -0600

    install eslint

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import eslintPluginAstro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
+
+export default [
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      astro: eslintPluginAstro,
+    },
+  },
+  {
+    files: ["**/*.mjs", "**/*.cjs"],
+    languageOptions: {
+      globals: {
+        require: "readonly",
+      },
+    },
+  },
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+];
