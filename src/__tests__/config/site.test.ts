bc4967bff828994d58926a407195ee4c6acf119c
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2025-01-28 11:54:03 -0600

    fix: fix sight test

--- src/__tests__/config/site.test.ts
+++ src/__tests__/config/site.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
-import config, { type SiteConfig } from '../site';
+import config, { type SiteConfig } from '../../config/site';
 
 describe('Site Configuration', () => {
   beforeEach(() => {
