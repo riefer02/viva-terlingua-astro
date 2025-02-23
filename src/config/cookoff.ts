e500ff30effafd2d3a8f98266d1986461ac834fd
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2025-02-23 17:59:52 -0600

    feat: refactor hero

---
+++ src/config/cookoff.ts
@@ -0,0 +1,31 @@
+export interface CookoffConfig {
+  dates: {
+    start: string;
+    end: string;
+    year: number;
+  };
+  location: {
+    name: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+}
+
+const config: CookoffConfig = {
+  dates: {
+    start: 'Oct 30th',
+    end: 'Nov 2nd',
+    year: new Date().getFullYear(),
+  },
+  location: {
+    name: 'Terlingua',
+    address: 'Behind the Store',
+    city: 'Terlingua',
+    state: 'TX',
+    zip: '79852',
+  },
+} as const;
+
+export default config;
